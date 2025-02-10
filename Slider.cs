854de342f1b04c9a3be484895ea6a7ba0e7e78dd
Tati <iced.tati@gmail.com>
2025-02-10 18:14:58 -0300

    slider now hooked up

--- Slider.cs
+++ Slider.cs
@@ -1,24 +1,39 @@
+using APC;
+using APCGear.APCOut;
 using Godot;
 using System;
 
 public partial class Slider : VSlider
 {
 	[Export]
-	public int id { get; set; } = 0;
+	public sliders id { get; set; } = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		this.AddToGroup("subscribed_to_store");
-	}
+        Bus.Subscribe<SliderChangedEvent, SliderChangedEventArgs>(resolve_event);
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 	public void _on_check_box_toggled(bool muted)
 	{
 		this.Editable = !muted;
 		//TODO send mute 
+	}
+
+	public void changeValue(double newValue)
+	{
+		this.Value = newValue;
+	}
+
+	public void resolve_event(SliderChangedEventArgs action)
+	{
+		if (action.id == (int)id)
+		{
+			CallDeferred("changeValue", Convert.ToDouble(action.value));		
+		}
 	}
 }
