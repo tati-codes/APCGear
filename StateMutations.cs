e6564e76f79b0fcfe57ec77c1f1a65ba29116979
Tati <iced.tati@gmail.com>
2025-02-12 16:56:37 -0300

    deleted cycleled, added advancecolor, setbuttoncolor

--- StateMutations.cs
+++ StateMutations.cs
@@ -8,36 +8,58 @@ using System.Reflection.Metadata.Ecma335;
 using state_types;
 using APCGear.KeyAction;
 using WindowsInput;
+using APCGear.APCOut;
+using APCGear.Color;
 
 public partial class State : Node
 {
-    public void cycleLed(CycleLedEventArgs ev)
+    public void enactShortcut(ShortcutEventArgs shortcutEventArgs)
     {
-        var btn = buttons[ev.id];
-        var max_length = Enum.GetValues(btn.type == btn_type.INNER ? typeof(inner_state) : typeof(outer_state)).Length - 1;
-        if (btn.color == max_length)
+        Keyboard.simulateKeypress(shortcutEventArgs.Hotkey);
+    }
+    public void advanceColor(BtnReleasedEventArgs args)
+    {
+        btn_state btn = buttons[args.Id];
+        if (btn.colorTransitions == null) return;
+        int max_length = btn.colorTransitions.sequence.Length - 1;
+        var seq = btn.colorTransitions.sequence;
+        int current = btn.colorTransitions.current;
+        if (current == max_length)
         {
-            btn.color = 0;
-        } else
-        {
-            btn.color += 1;
+            btn.colorTransitions.current = 0;
         }
-        buttons[ev.id] = btn;
+        else
+        {
+            btn.colorTransitions.current += 1;
+        }
+        buttons[args.Id] = btn;
         switch (btn.type)
         {
             case btn_type.INNER:
-                Pad.Instance.handler.set_inner(ev.id, (inner_state)btn.color);
+                Pad.Instance.handler.set_inner(args.Id, (inner_state)btn.colorTransitions.currentColor);
                 break;
             case btn_type.OUTER:
-                Pad.Instance.handler.set_outer((APC.outer_btns)ev.id, (outer_state)btn.color);
+                Pad.Instance.handler.set_outer((APC.outer_btns)args.Id, (outer_state)btn.colorTransitions.currentColor);
                 break;
             default:
                 break;
         }
     }
-    public void enactShortcut(ShortcutEventArgs shortcutEventArgs)
+    public void pasteText(TextEventArgs args) => Keyboard.pasteText(args.text);
+    public void setButtonColor(ColorTransitions transitions)
     {
-        Keyboard.simulateKeypress(shortcutEventArgs.Hotkey);
+        btn_state btn = selected;
+        switch (btn.type)
+        {
+            case btn_type.INNER:
+                Pad.Instance.handler.set_inner(selected_btn, (inner_state)transitions.currentColor);
+                break;
+            case btn_type.OUTER:
+                Pad.Instance.handler.set_outer((APC.outer_btns)selected_btn, (outer_state)transitions.currentColor);
+                break;
+            default:
+                break;
+        }
     }
 }
 
