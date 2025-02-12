f4afc48ec6e2407c21754c96334a5ef958fb54a1
Tati <iced.tati@gmail.com>
2025-02-12 16:57:35 -0300

    hooked up new events and such

--- State.cs
+++ State.cs
@@ -2,12 +2,14 @@ using APC;
 using APCGear;
 using APCGear.APCIn;
 using APCGear.APCOut;
+using APCGear.Color;
 using APCGear.KeyAction;
 using APCGear.UI;
 using Godot;
 using state_types;
 using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 
 public partial class State : Node
 {
@@ -29,20 +31,17 @@ public partial class State : Node
         var outer = Enum.GetValues<outer_btns>();
         for (int i = 0; i < 64; i++)
         {
-            buttons.Add(i, new state_types.btn_state(state_types.btn_type.INNER, 0, state_types.btn_action.LED_CYCLE));
+            buttons.Add(i, new state_types.btn_state(state_types.btn_type.INNER, state_types.btn_action.NONE));
         }
         foreach (var item in outer)
         {
-            buttons.Add((int)(item), new state_types.btn_state(state_types.btn_type.OUTER, 0, state_types.btn_action.LED_CYCLE));
+            buttons.Add((int)(item), new state_types.btn_state(state_types.btn_type.OUTER, state_types.btn_action.NONE));
         }
         Bus.Subscribe<BtnSelectedEvent, BtnSelectedEventArgs>((BtnSelectedEventArgs args) => { this.selected_btn = args.id; });
         Bus.Subscribe<BtnReleasedEvent, BtnReleasedEventArgs>((BtnReleasedEventArgs args) => {
             var btn = buttons[args.Id];
             switch (btn.action)
             {
-                case state_types.btn_action.LED_CYCLE:
-                    Bus.Publish<CycleLedEvent, CycleLedEventArgs>(new CycleLedEventArgs() { id = args.Id });
-                    break;
                 case state_types.btn_action.SHORTCUT:
                     if (btn.hotkey != null) {
                         Hotkey hotkey = btn.hotkey.Value;
@@ -51,10 +50,18 @@ public partial class State : Node
                     break;
                 case state_types.btn_action.OBS:
                     break;
+
+                case state_types.btn_action.TEXT:
+                    if (btn.text != null)
+                    {
+                        Bus.Publish<TextEvent, TextEventArgs>(new() { text = btn.text });
+                    }
+                    break;
                 default:
                     break;
             }
         });
+        Bus.Subscribe<BtnReleasedEvent, BtnReleasedEventArgs>(advanceColor);
         Bus.Subscribe<ShortcutChanged, ShortcutEventArgs>((args) =>
         {
             btn_state state = this.buttons[selected_btn];
@@ -62,9 +69,22 @@ public partial class State : Node
             state.action = btn_action.SHORTCUT;
             this.buttons[selected_btn] = state;
         });
-        Bus.Subscribe<CycleLedEvent, CycleLedEventArgs>(cycleLed);
+        Bus.Subscribe<SetColorEvent, ColorTransitions>((args) =>
+        {
+            btn_state state = this.buttons[selected_btn];
+            state.colorTransitions = args;
+            this.buttons[selected_btn] = state;
+        });
         Bus.Subscribe<ShortcutEvent, ShortcutEventArgs>(enactShortcut);
-
+        Bus.Subscribe<TextEvent, TextEventArgs>(pasteText);
+        Bus.Subscribe<SetTextEvent, TextEventArgs>((args) =>
+        {
+            btn_state state = this.buttons[selected_btn];
+            state.text = args.text;
+            state.action = btn_action.TEXT;
+            this.buttons[selected_btn] = state;
+        });
+        Bus.Subscribe<ImmediateColorChangeEvent, ColorTransitions>(setButtonColor);
     }
 }
 
@@ -77,22 +97,35 @@ namespace state_types
     }
     public enum btn_action
     {
-        LED_CYCLE,
         SHORTCUT,
         OBS,
+        TEXT,
+        NONE,
     }
-    public struct btn_state
+    public class btn_state
     {
         public btn_type type;
-        public int color;
         public btn_action action;
         public Hotkey? hotkey;
-        public btn_state(btn_type t, int c, btn_action a)
+        public ColorTransitions? colorTransitions;
+        public string? label;
+        public string? text;
+        public btn_state(btn_type t, btn_action a)
         {
             type = t;
-            color = c;
             action = a;
             hotkey = null;
+            switch (t)
+            {
+                case btn_type.INNER:
+                    colorTransitions = ColorTransitions.inner_solids;
+                    break;
+                case btn_type.OUTER:
+                    colorTransitions = ColorTransitions.outer_solids;
+                    break;
+                default:
+                    break;
+            }
         }
 
     }
