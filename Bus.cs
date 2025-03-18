6d94a38e3b4979033f1eee1603b335e0a77f9473
Tati <iced.tati@gmail.com>
2025-03-18 16:11:55 -0300

    added a debug routine to the event bus

--- Bus.cs
+++ Bus.cs
@@ -1,11 +1,16 @@
-using APCGear;
-using APCGear.APCIn;
+using APCEvents;
+using APCEvents.APCIn;
+using APCEvents.Debug;
+using APCGear.Audio;
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
 
 public partial class Bus : Node
 {
+
     private static readonly Dictionary<Type, Func<Object>> _mapping = new Dictionary<Type, Func<Object>>() {
         //[typeof(CycleLedEvent)] = () => CycleLedEvent.instance,
     };
@@ -32,12 +37,34 @@ public partial class Bus : Node
         //GD.Print(presEvent);
         presEvent.Subscribe(action);
     }
+    public static string debugStringify(Object t) {
+        Type myClassType = t.GetType();
+        PropertyInfo[] properties = myClassType.GetProperties();
+        if (myClassType.ToString() == "APCGear.Audio.AudioSessionsRefreshArgs")
+        {
+            return "";
+        }
+        string result = "{ \n";
+        foreach (PropertyInfo property in properties)
+        {
+            result += property.Name + ": " + property.GetValue(t, null) + "\n";
+        }
+        result += "}\n";
+        GD.Print(result);
+        return result;
+    }
     public static void Publish<T, APCEventArgs>(APCEventArgs args) where T : APCEvent<APCEventArgs>, new()
         where APCEventArgs : IAPCArgs
     {
         var presEvent = GetEvent<T, APCEventArgs>();
-        GD.Print("Firing: ",presEvent);
+        var debugEvent = GetEvent<APCEvents.Debug.DbgEvent, DebugString>();
+        if (presEvent.ToString() != "APCGear.Audio.AudioSessionsRefresh") GD.Print("Firing: ", presEvent);
         presEvent.Publish(args);
+        debugEvent.Publish(new DebugString()
+        {
+            name = presEvent.ToString(),
+            args = debugStringify(args)
+        });
     }
 
     public override void _Ready()
